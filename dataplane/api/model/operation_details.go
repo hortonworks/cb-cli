be167d4f7fb1e9300c3c7b6e41613f16c45aeb9f
bbihari <bbihari@hortonworks.com>
2019-10-04 15:14:59 +0200

    QAINFRA-9360 Cluster creation: freeipa error since mow-int upgrade, fix api

--- dataplane/api/model/operation_details.go
+++ dataplane/api/model/operation_details.go
@@ -53,6 +53,9 @@ type OperationDetails struct {
 	// user name
 	UserName string `json:"userName,omitempty"`
 
+	// utcdate time
+	UtcdateTime string `json:"utcdateTime,omitempty"`
+
 	// uuid
 	UUID string `json:"uuid,omitempty"`
 
