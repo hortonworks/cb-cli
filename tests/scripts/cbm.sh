d5f7dbaac1c12b1e83cbbe54941dc2790c1d2fbf
Annamaria Szegedi <aszegedi@hortonworks.com>
2020-02-13 16:07:25 +0100

    CB-5525 RC 2.9 pull request integration test docker-compose init

--- tests/scripts/cbm.sh
+++ tests/scripts/cbm.sh
@@ -3,6 +3,26 @@
 : ${GIT_VERSION:=latest}
 : ${BASE_URL:=https://127.0.0.1}
 
+compose-init() {
+    declare desc="Installing Docker-Compose 1.21.2 if it is no present on machine"
+
+    local required_compose=1.21.2
+    local compose_version=$(docker-compose --version 2>&1 | grep -E -o "([0-9]+\.)+[0-9]+")
+    local compose_binary=$(which docker-compose)
+
+    if [[ -z "${compose_version}" ]]; then
+      echo $TERM
+      echo "$(tty -s && tput setaf 3)* Docker-Compose required, installing with ${required_compose} version...$(tty -s && tput sgr 0)"
+      sudo curl -L "https://github.com/docker/compose/releases/download/${required_compose}/docker-compose-$(uname -s)-$(uname -m)"  -o /usr/local/bin/docker-compose
+      sudo mv /usr/local/bin/docker-compose /usr/bin/docker-compose
+      sudo chmod +x /usr/bin/docker-compose
+    else
+      echo $TERM
+      echo "$(tty -s && tput setaf 3)* Already installed Compose version: ${compose_version}"
+      echo "* Already installed Compose binary: ${compose_binary}$(tty -s && tput sgr 0)"
+    fi
+}
+
 mock-image-tag() {
     declare desc="Set Cloudbreak Mock Docker image tag"
 
@@ -36,6 +56,7 @@ mock-start() {
 }
 
 main() {
+    compose-init
     mock-image-tag
     mock-start
     mock-start-logs
