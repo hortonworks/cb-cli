9810927441ae735cff74be97dfee4251e9728672
Jamison Bennett <jamison.bennett@gmail.com>
2020-03-03 14:11:35 -0600

    CB-5159: Update the environment swagger API
    Update the environment swagger API to allow selecting the number of
    FreeIPA instances.

--- dataplane/api-environment/model/simple_environment_v1_response.go
+++ dataplane/api-environment/model/simple_environment_v1_response.go
@@ -53,6 +53,9 @@ type SimpleEnvironmentV1Response struct {
 	// Enum: [CREATION_INITIATED DELETE_INITIATED UPDATE_INITIATED ENVIRONMENT_VALIDATION_IN_PROGRESS NETWORK_CREATION_IN_PROGRESS NETWORK_DELETE_IN_PROGRESS RDBMS_DELETE_IN_PROGRESS FREEIPA_CREATION_IN_PROGRESS FREEIPA_DELETE_IN_PROGRESS CLUSTER_DEFINITION_CLEANUP_PROGRESS UMS_RESOURCE_DELETE_IN_PROGRESS IDBROKER_MAPPINGS_DELETE_IN_PROGRESS S3GUARD_TABLE_DELETE_IN_PROGRESS DATAHUB_CLUSTERS_DELETE_IN_PROGRESS DATALAKE_CLUSTERS_DELETE_IN_PROGRESS PUBLICKEY_CREATE_IN_PROGRESS PUBLICKEY_DELETE_IN_PROGRESS AVAILABLE ARCHIVED CREATE_FAILED DELETE_FAILED UPDATE_FAILED STOP_DATAHUB_STARTED STOP_DATAHUB_FAILED STOP_DATALAKE_STARTED STOP_DATALAKE_FAILED STOP_FREEIPA_STARTED STOP_FREEIPA_FAILED ENV_STOPPED START_DATAHUB_STARTED START_DATAHUB_FAILED START_DATALAKE_STARTED START_DATALAKE_FAILED START_FREEIPA_STARTED START_FREEIPA_FAILED]
 	EnvironmentStatus string `json:"environmentStatus,omitempty"`
 
+	// The FreeIPA paramaters
+	FreeIpa *FreeIpaResponse `json:"freeIpa,omitempty"`
+
 	// IDBroker mapping source.
 	// Enum: [NONE MOCK IDBMMS]
 	IDBrokerMappingSource string `json:"idBrokerMappingSource,omitempty"`
@@ -109,6 +112,10 @@ func (m *SimpleEnvironmentV1Response) Validate(formats strfmt.Registry) error {
 	}
 
 	if err := m.validateEnvironmentStatus(formats); err != nil {
+		res = append(res, err)
+	}
+
+	if err := m.validateFreeIpa(formats); err != nil {
 		res = append(res, err)
 	}
 
@@ -341,6 +348,24 @@ func (m *SimpleEnvironmentV1Response) validateEnvironmentStatus(formats strfmt.R
 	// value enum
 	if err := m.validateEnvironmentStatusEnum("environmentStatus", "body", m.EnvironmentStatus); err != nil {
 		return err
+	}
+
+	return nil
+}
+
+func (m *SimpleEnvironmentV1Response) validateFreeIpa(formats strfmt.Registry) error {
+
+	if swag.IsZero(m.FreeIpa) { // not required
+		return nil
+	}
+
+	if m.FreeIpa != nil {
+		if err := m.FreeIpa.Validate(formats); err != nil {
+			if ve, ok := err.(*errors.Validation); ok {
+				return ve.ValidateName("freeIpa")
+			}
+			return err
+		}
 	}
 
 	return nil
